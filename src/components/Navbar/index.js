a3115cc3216289d1ea4b439405faed9361da8355
Junaid <intelpik@gmail.com>
2021-04-10 12:05:54 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,10 +3,7 @@ import { IconContext } from "react-icons/lib";
 import { animateScroll as scroll } from "react-scroll";
 import {  useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+
 import {
   Nav,
   NavLink,
@@ -18,57 +15,9 @@ import {
   UserNameTag
 } from "./Navbar.elements";
 import decode from 'jwt-decode'; 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
- 
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 1),
-    height: '100%',
-    color:'#fff',
-    fontSize:'20px',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1.5, 1.5, 1.5, 0),
-    color:'#fff',
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '10ch',
-      '&:focus': {
-        width: '180px',
-      },
-    },
-  },
-}));
 
 const Navbar = ({ toggle }) => {
-    const classes = useStyles();
+    
   
   const [scrollNav, setScrollNav] = useState(false);
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
@@ -129,23 +78,7 @@ const Navbar = ({ toggle }) => {
               </NavItem>
               : null}
               
-              <NavItem>
-                <NavLink to="/">
-                     <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-                </NavLink>
-              </NavItem>
+             
               <NavItem>
                 <NavLink to="/cart">
                   <img
